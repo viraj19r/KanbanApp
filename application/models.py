a59991021322be39ed6a70bad8aba7c6f7c7dfb3
Viraj Sharma <viraj19r@gmail.com>
2022-08-12 16:03:19 +0530

    create models role,user and roleuser relationships

--- application/models.py
+++ application/models.py
@@ -1,9 +1,30 @@
 from enum import unique
 from application.database import db
-from flask_security import UserMixin
+from flask_security import UserMixin,RoleMixin
+
+
+
+class RolesUsers(db.Model):
+    __tablename__ = 'roles_users'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('role.id'))
+
 
 class User(db.Model,UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer(),primary_key=True)
+    fname = db.Column(db.String())
+    lname = db.Column(db.String())
     email = db.Column(db.String(80),unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary='roles_users',backref=db.backref('users', lazy='dynamic'))
+
+class Role(db.Model, RoleMixin):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
