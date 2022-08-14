da15af2f4d53e5fbb84930f78aedd8b08eb9035d
Viraj Sharma <viraj19r@gmail.com>
2022-08-14 17:03:24 +0530

    setup controller and validation for sign in and register

--- application/controllers.py
+++ application/controllers.py
@@ -1,7 +1,25 @@
 from application.__init__ import app
-from flask import render_template
-
+from flask import render_template, url_for
+from flask import request,flash, redirect
+from application.forms import RegistrationForm,LoginForm
 
 @app.route("/")
 def hello():
-    return render_template("register.html")
+    return render_template("signin.html", index="viraj")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.fname.data} {form.lname.data} successfully','success')
+        return redirect(url_for('index.html'))
+    return render_template("register.html",form=form)
+        
+
+@app.route("/signin", methods=["GET","POST"])
+def signin():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'{form.email.data} logged in successfully!','success')
+        return redirect(url_for('register'))
+    return render_template("signin.html", form=form)
