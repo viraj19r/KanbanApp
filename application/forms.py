e3fbccc162a589f901a2dc9a671778f53887fffa
Viraj Sharma <viraj19r@gmail.com>
2022-08-14 17:03:54 +0530

    create flask wtf forms for signin and registration

---
+++ application/forms.py
@@ -0,0 +1,25 @@
+from ast import Pass
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,Length,Email,EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    fname = StringField('First Name',validators=[DataRequired(),Length(min=2, max=25)])
+    lname = StringField('Last Name',validators=[DataRequired(),Length(min=2, max=25)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+
+
+
+
+
