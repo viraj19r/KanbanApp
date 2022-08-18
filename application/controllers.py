2048b5a2c4dff9b823675810c2412e0e4b534179
Viraj Sharma <viraj19r@gmail.com>
2022-08-18 20:37:48 +0530

    add crud on list,cards

--- application/controllers.py
+++ application/controllers.py
@@ -1,4 +1,5 @@
 import email
+from os import stat
 from turtle import title
 from application import app,db,bcrypt
 from flask import render_template, url_for
@@ -16,7 +17,6 @@ from datetime import datetime
 @login_required 
 def board():
     lists = List.query.all()
-    
     return render_template("board.html",lists=lists)
 
 @app.route("/register", methods=["GET","POST"])
@@ -31,7 +31,6 @@ def register():
         return redirect(url_for('login'))
     return render_template("register.html",form=form)
         
-
 @app.route("/login", methods=["GET","POST"])
 def login():
     form = LoginForm()
@@ -45,8 +44,8 @@ def login():
             flash('Login unsuccessful! Please check your credentials again','danger')
     return render_template("signin.html", form=form)
 
-
 @app.route("/logout", methods=["GET","POST"])
+@login_required 
 def logout():
     logout_user()
     flash('Logout successfully, Try logging in again','success')
@@ -56,20 +55,26 @@ def logout():
 @login_required 
 def add_list():
     form = CreateList()
-    if form.validate_on_submit():
-        list = List(name=form.list_name.data,description=form.description.data,user_id=current_user.id)
+    all_lists = List.query.all()
+    if len(all_lists) < 5 :
+        if form.validate_on_submit():
+            list = List(name=form.list_name.data,description=form.description.data,user_id=current_user.id)
 
-        checkdblist = List.query.filter_by(name=form.list_name.data).first()
-        if not  checkdblist:
-            db.session.add(list)
-            db.session.commit()
-            flash(f'List {form.list_name.data} has been created successfully','success')
+            checkdblist = List.query.filter_by(name=form.list_name.data).first()
+            if not  checkdblist:
+                db.session.add(list)
+                db.session.commit()
+                flash(f'List {form.list_name.data} has been created successfully','success')
+                return redirect(url_for('board'))
+            else:
+                flash(f'List named {form.list_name.data} already exist, Please try again with different name','success')
+    else:
+            flash(f'You cannot create more than 5 Lists','warning')
             return redirect(url_for('board'))
-        else:
-            flash(f'List named {form.list_name.data} already exist, Please try again with different name','success')
     return render_template("add_list.html",form=form)
 
 @app.route("/add_card", methods=["GET","POST"])
+@login_required
 def add_card():
     form = CreateCard()
     #send list to form choices
@@ -86,10 +91,100 @@ def add_card():
         if not checkdbcard:
             db.session.add(card)
             db.session.commit()
-            flash(f'List {form.title.data} has been created successfully','success')
+            flash(f'Card {form.title.data} has been created successfully','success')
             return redirect(url_for('board'))
         else:
             flash(f'Card named "{form.list_name.data}" already exist, Please try again with different name','success')
     return render_template("add_card.html",form=form)
+
+@app.route("/edit_list/<int:list_id>", methods=["GET","POST"])
+@login_required 
+def edit_list(list_id):
+    form = CreateList()
+    list = List.query.filter_by(id=list_id).first()
+    if request.method == 'GET':
+        form.list_name.data = list.name
+        form.description.data = list.description
+    if form.validate_on_submit():
+        checkdblist = List.query.filter_by(name=form.list_name.data).first()
+        if not  checkdblist:
+            list.name = form.list_name.data
+            list.description = form.description.data
+            db.session.commit()
+            flash(f'List updated successfully','success')
+            return redirect(url_for('board'))
+        else:
+            flash(f'List named {form.list_name.data} already exist, Please try again with different name','success')
+    return render_template("edit_list.html",form=form)
+
+
+@app.route("/edit_card/<int:card_id>", methods=["GET","POST"])
+@login_required
+def edit_card(card_id):
+    form = CreateCard()
+    #send list to form choices
+    lists = List.query.filter_by(user_id=current_user.id).all()
+    form.choose_list.choices = [(list.id,list.name) for list in lists]
+    card = Card.query.filter_by(id=card_id).first()
+    if request.method == "GET":
+        form.title.data = card.title
+        form.content.data = card.content
+        form.deadline.data = card.deadline
+    if form.validate_on_submit():
+        status = False
+        complete_status = form.complete_status.data
+        if complete_status == 'Completed':
+            status = True
+        #check if that cardname exist in that particular list 
+        checkdbcard = Card.query.filter_by(title=form.title.data).first()
+        if not checkdbcard:
+            card.title = form.title.data
+            card.content = form.content.data
+            card.deadline = form.deadline.data
+            card.completed_status = status
+            card.list_id = form.choose_list.data
+            db.session.commit()
+            flash(f'Card updated successfully','success')
+            return redirect(url_for('board'))
+        else:
+            flash(f'Card named "{form.list_name.data}" already exist, Please try again with different name','success')
+    return render_template("edit_card.html",form=form)
+
+@app.route("/delete_list/<int:list_id>", methods=["GET","POST"])
+@login_required 
+def delete_list(list_id):
+    list = List.query.filter_by(id=list_id).first()
+    list_name = list.name
+    cards = list.cards
+    for card in cards:
+        db.session.delete(card)
+    db.session.delete(list)
+    db.session.commit()
+    flash(f'List named {list_name} deleted successfully','success')
+    return redirect(url_for('board'))
+
+
+@app.route("/delete_card/<int:card_id>", methods=["GET","POST"])
+@login_required
+def delete_card(card_id):
+    card = Card.query.filter_by(id=card_id).first()
+    card_title = card.title
+    db.session.delete(card)
+    db.session.commit()
+    flash(f'Card {card_title} deleted successfully','success')
+    return redirect(url_for('board'))
+
+
+
+@app.route("/mark_card_completed/<int:card_id>", methods=["GET","POST"])
+@login_required
+def mark_card_completed(card_id):
+    card = Card.query.filter_by(id=card_id).first()
+    card_title = card.title
+    card.completed_status = True
+    card.date_completed = datetime.now()
+    db.session.commit()
+    flash(f'Card "{card_title}" marked as completed','success')
+    return redirect(url_for('board'))
 
 
